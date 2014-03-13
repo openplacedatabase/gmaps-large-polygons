20631d72b3992d2bb4a0a535c416023eaec0e00a
Justin <justincyork@gmail.com>
2014-03-12 21:04:28 -0600

    Create EditPath
    This simplified many things by allowing us to operate on just one path
    at a time. It will also enable further simplification when we create
    EditLines.

--- gmaps-large-polygon.js
+++ gmaps-large-polygon.js
@@ -80,154 +80,174 @@
   /**
    * Create the edit lines
    */
-  polyProto.generateEditLines = function(){
+  polyProto.generateEditLines = function(){   
+    var self = this;
+    self.editPaths = [];
+    self.getPaths().forEach(function(path, pathIndex){
+      self.editPaths.push(new EditPath(self, path));
+    });
+  };
+  
+  /**
+   * EditPath is a group of edit lines that represent
+   * a single path from a polygon. This is given
+   * a reference to the original path so that we 
+   * can properly update it and a reference to 
+   * the original polygon so that we can dynamically 
+   * detect the path index and replace the old path
+   * with a new path when the edit lines are modified
+   */
+  var EditPath = function(polygon, path){
+    this.polygon = polygon;
+    this.path = path;
+    this.lines = [];
+    
+    this.createLines();
+  };
+  
+  /**
+   * Create the edit lines
+   */
+  EditPath.prototype.createLines = function(){
     
     var self = this,
-        map = self.getMap(),
+        polygon = self.polygon,
+        path = self.path,
+        lines = self.lines,
+        lineLengths = _lineLengths(path.getLength(), polygon.lineSize),
         lineStyle = {
-          strokeColor: self.get('strokeColor'),
-          strokeOpacity: self.get('strokeOpacity'),
-          strokeWeight: self.get('strokeWeight')
-        };
+          strokeColor: polygon.get('strokeColor'),
+          strokeOpacity: polygon.get('strokeOpacity'),
+          strokeWeight: polygon.get('strokeWeight')
+        },
+        map = polygon.getMap();
     
-    // Process each path independently
-    this.getPaths().forEach(function(path, pathIndex){
-
-      var lineLengths = _lineLengths(path.getLength(), self.lineSize),
-          lines = [];
+    // Setup each edit line
+    for(var position = 0, i = 0; i < lineLengths.length; i++){
       
-      // Iterate over each edit line
-      var position = 0;
-      for(var i = 0; i < lineLengths.length; i++){
-        var newLinePath = [],
-            end = Math.min(position + lineLengths[i], path.getLength());
-        
-        // Add the relevent points to the line's path
-        for(; position < end; position++){
-          newLinePath.push(path.getAt(position));
-        }
-        
-        // Decrement position by 1 so that the next
-        // line duplicates the last point of this line
-        position--;
-        
-        // Create the google maps line object
-        // and style it to match the polygon's borders
-        var line = new google.maps.Polyline();
-        line.setPath(newLinePath);
-        line.setOptions(lineStyle);
-        line.setMap(map);
-        
-        // Add events
-        line.addListener('click', function(){
-          _forEach(lines, function(i, line){
-            line.setEditable(false);
-          });
-          this.setEditable(true);
-        });
-        line.addListener('mouseover', function(){
-          this.set('strokeColor', self.highlightColor);
-        });
-        line.addListener('mouseout', function(){
-          this.setOptions(lineStyle);
-        });
-        
-        // Save the line
-        lines.push(line);
+      var newLinePath = [],
+          end = Math.min(position + lineLengths[i], path.getLength());
+      
+      // Add the relevent points to the line's path
+      for(; position < end; position++){
+        newLinePath.push(path.getAt(position));
       }
       
-      // Add the first point of the path to the end of the last line
-      lines[lines.length-1].getPath().push(path.getAt(0));
+      // Decrement position by 1 so that the next
+      // line duplicates the last point of this line
+      position--;
       
-      // Keep track of whether a point event was fired
-      // by the user or by us updating a neighbor point
-      var updatingNeighbor = false,
-          fireChangeEvent = true;
+      // Create the google maps line object
+      // and style it to match the polygon's borders
+      var line = new google.maps.Polyline();
+      line.setPath(newLinePath);
+      line.setOptions(lineStyle);
+      line.setMap(map);
       
-      // Setup event listeners on the line's paths so that
-      // we can update the underlying shape when the lines change
-      _forEach(lines, function(i, line){
-        
-        // New point
-        line.getPath().addListener('insert_at', function(){
-          self.updateFromLines(pathIndex, lines, fireChangeEvent);
+      // Add events
+      line.addListener('click', function(){
+        _forEach(lines, function(i, line){
+          line.setEditable(false);
         });
-        
-        // Point removed
-        line.getPath().addListener('remove_at', function(){
-          self.updateFromLines(pathIndex, lines, fireChangeEvent);
-        });
-        
-        // Point moved
-        line.getPath().addListener('set_at', function(vertex){
-          
-          // When either the first or last point was moved we need
-          // to move the matching point of the neighboring line.
-          // Because lines may be deleted (unlikely, but possible)
-          // we have to figure out the line's position and neighbors
-          // at runtime.
-          if((vertex === 0 || vertex === this.getLength() - 1) && !updatingNeighbor){
-            var neighbor = findNeighbor(lines, line, vertex);
-            updatingNeighbor = true;
-            neighbor.path.setAt(neighbor.index, this.getAt(vertex));
-          } 
-          
-          // Update underlying shape with changes only if we're moving
-          // a point in the middle of the line or we're updating a neighbor
-          // point after the user moved a first/last point
-          else {
-            updatingNeighbor = false;
-            self.updateFromLines(pathIndex, lines, fireChangeEvent);
-          }
-        });
-        
-        // Deleting a point when rightclicked.
-        // updateFromLines is not called here to update the polygon
-        // because we modify the lines which fire the event listeners
-        // bound above which then call updateFromLines
-        // Inspired by http://stackoverflow.com/a/14441786
-        line.addListener('rightclick', function(event){
-          var vertex = event.vertex;
-          
-          if(vertex != null){
-            
-            // Remove the point
-            this.getPath().removeAt(event.vertex);
-            
-            // If the point being removed is the first or last
-            // point in the line then we need to find and move
-            // the neigbor's point to match our new end point.
-            if(vertex === 0 || vertex === this.getPath().getLength()){
-              var neighbor = findNeighbor(lines, this, vertex),
-                  newEndIndex = vertex === 0 ? 0 : this.getPath().getLength() - 1;
-              fireChangeEvent = false;
-              neighbor.path.setAt(neighbor.index, this.getPath().getAt(newEndIndex));
-              fireChangeEvent = true;
-            }
-            
-            // If there is only one point left then delete the line.
-            // It's neghbors should both be overlapping now.
-            if(this.getPath().getLength() === 1){
-              this.setMap(null);
-              var linePos = lines.indexOf(this);
-              lines.splice(linePos, 1);
-            }            
-
-            // Delete the entire path if there is less than
-            // three points left; with only one or two points
-            // you don't have a polygon
-            if(self.getPaths().getAt(pathIndex).getLength() < 3){
-              self.getPaths().removeAt(pathIndex);
-              // If this path isn't the last one then it will change
-              // the ordering and make pathIndex irrelevent and break
-              // a lot of code
-            }
-          }
-        });
+        this.setEditable(true);
+      });
+      line.addListener('mouseover', function(){
+        this.set('strokeColor', polygon.highlightColor);
+      });
+      line.addListener('mouseout', function(){
+        this.setOptions(lineStyle);
       });
       
-      self.editLines = self.editLines.concat(lines);
+      // Save the line
+      lines.push(line);
+    }
+    
+    // Add the first point of the path to the end of the last line
+    lines[lines.length-1].getPath().push(path.getAt(0));
+    
+    // Keep track of whether a point event was fired
+    // by the user or by us updating a neighbor point
+    var updatingNeighbor = false,
+        fireChangeEvent = true;
+    
+    // Setup event listeners on the line's paths so that
+    // we can update the underlying shape when the lines change
+    _forEach(lines, function(i, line){
+      
+      // New point
+      line.getPath().addListener('insert_at', function(){
+        self.updateFromLines(fireChangeEvent);
+      });
+      
+      // Point removed
+      line.getPath().addListener('remove_at', function(){
+        self.updateFromLines(fireChangeEvent);
+      });
+      
+      // Point moved
+      line.getPath().addListener('set_at', function(vertex){
+        
+        // When either the first or last point was moved we need
+        // to move the matching point of the neighboring line.
+        // Because lines may be deleted (unlikely, but possible)
+        // we have to figure out the line's position and neighbors
+        // at runtime.
+        if((vertex === 0 || vertex === this.getLength() - 1) && !updatingNeighbor){
+          var neighbor = findNeighbor(lines, line, vertex);
+          updatingNeighbor = true;
+          neighbor.path.setAt(neighbor.index, this.getAt(vertex));
+        } 
+        
+        // Update underlying shape with changes only if we're moving
+        // a point in the middle of the line or we're updating a neighbor
+        // point after the user moved a first/last point
+        else {
+          updatingNeighbor = false;
+          self.updateFromLines(fireChangeEvent);
+        }
+      });
+      
+      // Deleting a point when rightclicked.
+      // updateFromLines is not called here to update the polygon
+      // because we modify the lines which fire the event listeners
+      // bound above which then call updateFromLines
+      // Inspired by http://stackoverflow.com/a/14441786
+      line.addListener('rightclick', function(event){
+        var vertex = event.vertex;
+        
+        if(vertex != null){
+          
+          // Remove the point
+          this.getPath().removeAt(event.vertex);
+          
+          // If the point being removed is the first or last
+          // point in the line then we need to find and move
+          // the neigbor's point to match our new end point.
+          if(vertex === 0 || vertex === this.getPath().getLength()){
+            var neighbor = findNeighbor(lines, this, vertex),
+                newEndIndex = vertex === 0 ? 0 : this.getPath().getLength() - 1;
+            fireChangeEvent = false;
+            neighbor.path.setAt(neighbor.index, this.getPath().getAt(newEndIndex));
+            fireChangeEvent = true;
+          }
+          
+          // If there is only one point left then delete the line.
+          // It's neghbors should both be overlapping now.
+          if(this.getPath().getLength() === 1){
+            this.setMap(null);
+            var linePos = lines.indexOf(this);
+            lines.splice(linePos, 1);
+          }            
 
+          // Delete the entire path if there is less than
+          // three points left; with only one or two points
+          // you don't have a polygon
+          var pathIndex = self.getPathIndex();
+          if(polygon.getPaths().getAt(pathIndex).getLength() < 3){
+            polygon.getPaths().removeAt(pathIndex);
+          }
+        }
+      });
     });
   };
   
@@ -236,10 +256,10 @@
    * path of the shape based on the lines that were
    * derived from it
    */
-  polyProto.updateFromLines = function(pathIndex, lines, fireEvent){
+  EditPath.prototype.updateFromLines = function(fireEvent){
     var newPoints = [];
-    for(var i = 0; i < lines.length; i++){
-      var linePath = lines[i].getPath(),
+    for(var i = 0; i < this.lines.length; i++){
+      var linePath = this.lines[i].getPath(),
           lineLength = linePath.getLength();
       linePath.forEach(function(point, j){
         // Don't add the last point of each line
@@ -249,10 +269,29 @@
         }
       });
     }
-    this.getPaths().setAt(pathIndex, new google.maps.MVCArray(newPoints));
+    // Get the index of the old path before we save the new one
+    var pathIndex = this.getPathIndex();
+    this.path = new google.maps.MVCArray(newPoints)
+    this.polygon.getPaths().setAt(pathIndex, this.path);
     if(fireEvent){
-      google.maps.event.trigger(this, 'changed');
+      google.maps.event.trigger(this.polygon, 'changed');
     }
+  };
+  
+  /**
+   * Calculate index of the EditPath's original
+   * path in the polygon. We do it dynamically to
+   * allow for paths to be deleted during editing.
+   */
+  EditPath.prototype.getPathIndex = function(){
+    var self = this,
+        pathIndex;
+    self.polygon.getPaths().forEach(function(path, i){
+      if(path === self.path){
+        pathIndex = i;
+      }
+    });
+    return pathIndex;
   };
   
   /**
